4dbfed104989212c2837a7b83b8f8d4275de1b80
dleo2021 <danleonard@hotmail.co.uk>
2024-09-11 21:09:24 +0100

    Creation of Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Navbar } from "./Components/Navbar";
 
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
